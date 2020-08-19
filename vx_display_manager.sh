7e3ed40d91c49701ce6e4acda234d1b4c2a61ab0
tanner-verkada <65693780+tanner-verkada@users.noreply.github.com>
2020-08-18 18:59:12 -0700

    Update vx_display_manager.sh

--- vx_display_manager.sh
+++ vx_display_manager.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-curl -o DisplayManager-1.0.2.dmg https://github.com/univ-of-utah-marriott-library-apple/display_manager/releases/download/1.0.2/DisplayManager-1.0.2.dmg
+curl -o /Users/verkada/DisplayManager-1.0.2.dmg https://github.com/univ-of-utah-marriott-library-apple/display_manager/releases/download/1.0.2/DisplayManager-1.0.2.dmg
 
 sleep 1
 
-hdiutil mount DisplayManager-1.0.2.dmg
+hdiutil mount /Users/verkada/DisplayManager-1.0.2.dmg
 
 sleep 1
 
@@ -12,9 +12,9 @@ installer -pkg /Volumes/Display\ Manager/DisplayManager-1.0.2.pkg -target /
 
 sleep 1
 
-display_manager.py res 2048 1280
+display_manager.py res 1920 1080
 
-sleep 2
+sleep 1
 
 installer -pkg /Volumes/Display\ Manager/Uninstall.pkg -target /
 
