2720a1588967107aedd827923dac053642552bdd
tanner-verkada <65693780+tanner-verkada@users.noreply.github.com>
2020-08-18 19:16:02 -0700

    Create vx_display_manager_2.sh

---
+++ vx_display_manager_2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl -L -o /Users/verkada/DisplayManager-1.0.2.dmg https://github.com/univ-of-utah-marriott-library-apple/display_manager/releases/download/1.0.2/DisplayManager-1.0.2.dmg
+
+hdiutil mount /Users/verkada/DisplayManager-1.0.2.dmg
+
+installer -pkg /Volumes/Display\ Manager/DisplayManager-1.0.2.pkg -target /
+
+display_manager.py res 1920 1080
+
+installer -pkg /Volumes/Display\ Manager/Uninstall.pkg -target /
+
+hdiutil unmount /Volumes/Display\ Manager
